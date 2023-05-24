b1feabed858615d50e6cbfcd9702d94b71d46a78
Momen Khaled <momenk208@gmail.com>
2023-05-24 17:21:16 +0300

    ...

--- src/connection/connect.js
+++ src/connection/connect.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //http://localhost:4000
 const http = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://relieved-frog-gloves.cyclic.app/api/v1",
 });
 
 export default http;
