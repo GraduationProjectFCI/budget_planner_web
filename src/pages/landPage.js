11793e0678cb552c2ba86ad06488ec3fe129f429
Momen Khaled <momenk208@gmail.com>
2023-06-19 06:42:35 +0300

    Add data visulaization

--- src/pages/landPage.js
+++ src/pages/landPage.js
@@ -1,7 +1,18 @@
 const LandPage = () => {
   return (
     <div>
-      <h1>LandPage</h1>
+      <iframe
+        style={{
+          background: "#F1F5F4",
+          border: "none",
+          borderRadius: "2px",
+          boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2)",
+          width: "100vw",
+          height: "100vh",
+        }}
+        title="Task Manager Dashboard"
+        src="https://charts.mongodb.com/charts-task-manager-tkjls/embed/dashboards?id=0608d54e-2393-4cf6-a5d2-34b0d17174ed&theme=light&autoRefresh=true&maxDataAge=3600&showTitleAndDesc=true&scalingWidth=scale&scalingHeight=scale"
+      ></iframe>
     </div>
   );
 };
