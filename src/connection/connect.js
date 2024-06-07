80dd4459874f64d3a1734aa51a35f0070c3e3305
Momen <momenk208@gmail.com>
2024-06-07 21:35:19 +0300

    update the API connection

--- src/connection/connect.js
+++ src/connection/connect.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //http://localhost:4000
 const http = axios.create({
-  baseURL: "https://relieved-frog-gloves.cyclic.app/api/v1",
+  baseURL: "https://budget-planner-api-1.onrender.com/api/v1",
 });
 
 export default http;
