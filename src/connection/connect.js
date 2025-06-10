2374d6f90eca1168e526408daad28dd14e3ab9dd
Momen Khaled <momenk208@gmail.com>
2025-06-10 10:50:51 +0300

    Update connect.js

--- src/connection/connect.js
+++ src/connection/connect.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //http://localhost:4000
 const http = axios.create({
-  baseURL: "https://budget-planner-api-fv87.onrender.com/api/v1",
+  baseURL: "https://budget-planner-api-kaah.onrender.com/api/v1",
 });
 
 export default http;
